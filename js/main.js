7146f5348e9db059d5fa04b5e255b07b5ebd4fc0
gonzalo-losanotos <LosantosGonzalo@hotmail.com>
2023-11-18 16:39:26 -0300

    18/11/2023 1.0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-const invocarArray = async () => {
+// Inicialización del objeto carrito
+const carrito = {
+    productos: [],
+    total: 0,
+};
 
+// Función para actualizar el carrito en el DOM
+function actualizarCarritoEnDOM() {
+    const carritoElement = document.getElementById("carritoElement"); // Elemento donde se mostrará el carrito
+
+    // Limpiar el carrito antes de actualizarlo
+    carritoElement.innerHTML = "";
+
+    // Recorrer los productos en el carrito y mostrarlos visualmente
+    carrito.productos.forEach(producto => {
+        const productoElement = document.createElement("div");
+        productoElement.classList.add("productoCarrito");
+
+        productoElement.innerHTML = `
+            <p>${producto.nombre} - $${producto.precio}</p>
+        `;
+
+        carritoElement.appendChild(productoElement);
+    });
+
+    // Actualizar el total del carrito
+    const totalElement = document.createElement("div");
+    totalElement.innerHTML = `<strong>Total: $${carrito.total}</strong>`;
+    carritoElement.appendChild(totalElement);
+}
+
+// Función invocarArray
+const invocarArray = async () => {
     try {
         const respuesta = await fetch("./zapatillas.json");
         const datos = await respuesta.json();
@@ -7,44 +38,35 @@ const invocarArray = async () => {
 
         console.log(datos);
 
+        cajita.innerHTML = ""; // Limpiar el contenedor antes de agregar elementos
+
         datos.productos.forEach(producto => {
-            const contenidoDiv = document.getElementById("contenedor");
-            const editarCajita = document.querySelectorAll(".cajaContenedora")
-            //cajitas
-            contenidoDiv.innerHTML += `
-            <div class="cajaPadre">
+            const div = document.createElement("div");
+            div.classList.add("cajaPadre");
+
+            div.innerHTML = `
                 <div class="card cajaContenedora" style="width: 20rem; z-index: 0;">
-
                     <img src="${producto.img}" class="card-img-top" alt="...">
-
                     <div class="card-body">
                         <h5 class="card-title">${producto.nombre}</h5>
                         <p class="card-text">$${producto.precio}</p>
-
-                        <button type="button" class="btn btn-primary btnPrincipal" id="btnPrincipal"> Añadir al carrito</button>
-
+                        <button type="button" class="btn btn-primary btnPrincipal"> Añadir al carrito</button>
                     </div>
-
                 </div>
-            </div>
             `;
+            
+            cajita.appendChild(div);
 
-            //evento click
-            const botones = document.querySelectorAll(".btnPrincipal");
+            // Agregar evento click al botón "Añadir al carrito" después de crearlo
+            div.querySelector(".btnPrincipal").addEventListener('click', () => {
+                const nombre = producto.nombre;
+                const precio = producto.precio;
+                const productoNuevo = { nombre, precio };
 
-            botones.forEach(boton => {
-                boton.addEventListener('click', () => {
-                    const divProducto = boton.closest(".card");
-                    const nombre = divProducto.querySelector(".card-title").textContent;
-                    const precioTexto = boton.querySelector(".card-text").textContent;
-                    const precio = parseFloat(precioTexto.replace(/\D/g, ''));
-                    const producto = { nombre, precio };
-
-                    carrito.productos.push(producto);
-                    carrito.total += precio;
-
-                    actualizarCarritoEnDOM();
-                });
+                // Agregar el producto al carrito y actualizar visualmente
+                carrito.productos.push(productoNuevo);
+                carrito.total += precio;
+                actualizarCarritoEnDOM();
             });
         });
 
@@ -52,5 +74,7 @@ const invocarArray = async () => {
         console.error('Se ha producido un error:', error);
     }
 }
+
+// Llamar a la función invocarArray
 invocarArray();
 
